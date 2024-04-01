b2ad519061e6830e0b2a21e9d5a04666cd4fff93
Martyushev Leoned <Lyonya.martyushev@mail.ru>
2024-04-01 18:04:11 +0500

    Initial commit

---
+++ src/test/java/ap/soft/test/fileparser/FileParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ap.soft.test.fileparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
